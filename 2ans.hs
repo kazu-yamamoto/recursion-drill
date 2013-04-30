852523fc74c97c77454cf55a3fca220e725e0e4c
Kazu Yamamoto <kazu@iij.ad.jp>
2013-04-30 13:31:46 +0900

    new style.

--- 2ans.hs
+++ 2ans.hs
@@ -5,87 +5,87 @@ import Small
 
 main :: IO ()
 main = hspec $ do
-    describe "soap" $
+    describe "my_soap_iter" $
       prop "behaves as model" $ \(Small n) -> n >= 0
-        ==> soap n == sum [0..n]
-    describe "fact" $
+        ==> my_soap_iter n == sum [0..n]
+    describe "my_fact_iter" $
       prop "behaves as model" $ \(Small n) -> n >= 1
-        ==> fact n == product [1..n]
-    describe "mul" $
+        ==> my_fact_iter n == product [1..n]
+    describe "my_mul_iter" $
       prop "behaves as model" $ \(Small m) (Small n) -> m >= 1 && n >= 1
-        ==> mul m n == m * n
-    describe "plus" $
+        ==> my_mul_iter m n == m * n
+    describe "my_plus_iter" $
       prop "behaves as model" $ \(Small m) (Small n) -> m >= 0 && n >= 0
-        ==> plus m n == m + n
-    describe "minus" $
+        ==> my_plus_iter m n == m + n
+    describe "my_minus_iter" $
       prop "behaves as model" $ \(Small m) (Small n) -> m >= 0 && n >= 0
-        ==> minus m n == m - n
-    describe "power" $
-      prop "behaves as model" $ \(Small m) (Small n) -> m >= 1 && n >= 1
-        ==> power m n == m ^ n
+        ==> my_minus_iter m n == m - n
+    describe "my_power_iter" $
+      prop "behaves as model" $ \(Small m) (Small n) -> m >= 1 && n >= 0
+        ==> my_power_iter m n == m ^ n
 
--- soap :: Int -> Int
--- soap 0 = 0
--- soap n = soap (n-1) + n
+-- my_soap :: Integer -> Integer
+-- my_soap 0 = 0
+-- my_soap n = soap (n-1) + n
 
-soap :: Int -> Int
-soap n = soap' n 0
+my_soap_iter :: Integer -> Integer
+my_soap_iter x = iter x 0
+  where
+    iter :: Integer -> Integer -> Integer
+    iter 0 acc = acc
+    iter n acc = iter (n-1) (acc + n)
 
-soap' :: Int -> Int -> Int
-soap' 0 acc = acc
-soap' n acc = soap' (n-1) (acc + n)
+-- my_fact :: Integer -> Integer
+-- my_fact 1 = 1
+-- my_fact n = fact (n - 1) * n
 
--- fact :: Int -> Int
--- fact 1 = 1
--- fact n = fact (n - 1) * n
+my_fact_iter :: Integer -> Integer
+my_fact_iter x = iter x 1
+  where
+    iter :: Integer -> Integer -> Integer
+    iter 1 acc = acc
+    iter n acc = iter (n - 1) (acc * n)
 
-fact :: Int -> Int
-fact n = fact' n 1
+-- my_mul :: Integer -> Integer -> Integer
+-- my_mul m 1 = m
+-- my_mul m n = mul m (n - 1) + m
 
-fact' :: Int -> Int -> Int
-fact' 1 acc = acc
-fact' n acc = fact' (n - 1) (acc * n)
+my_mul_iter :: Integer -> Integer -> Integer
+my_mul_iter x y = iter x y x
+  where
+    iter :: Integer -> Integer -> Integer -> Integer
+    iter _ 1 acc = acc
+    iter m n acc = iter m (n - 1) (acc + m)
 
--- mul :: Int -> Int -> Int
--- mul m 1 = m
--- mul m n = mul m (n - 1) + m
+-- my_plus :: Integer -> Integer -> Integer
+-- my_plus m 0 = m
+-- my_plus m n = plus m (n - 1) + 1
 
-mul :: Int -> Int -> Int
-mul m n = mul' m n m
+my_plus_iter :: Integer -> Integer -> Integer
+my_plus_iter x y = iter x y x
+  where
+    iter :: Integer -> Integer -> Integer -> Integer
+    iter _ 0 acc = acc
+    iter m n acc = iter m (n - 1) (acc + 1)
 
-mul' :: Int -> Int -> Int -> Int
-mul' _ 1 acc = acc
-mul' m n acc = mul' m (n - 1) (acc + m)
+-- my_minus :: Integer -> Integer -> Integer
+-- my_minus m 0 = m
+-- my_minus m n = minus m (n - 1) - 1
 
--- plus :: Int -> Int -> Int
--- plus m 0 = m
--- plus m n = plus m (n - 1) + 1
+my_minus_iter :: Integer -> Integer -> Integer
+my_minus_iter x y = iter x y x
+  where
+    iter :: Integer -> Integer -> Integer -> Integer
+    iter _ 0 acc = acc
+    iter m n acc = iter m (n - 1) (acc - 1)
 
-plus :: Int -> Int -> Int
-plus m n = plus' m n m
+-- my_power :: Integer -> Integer -> Integer
+-- my_power _ 0 = 1
+-- my_power m n = power m (n - 1) * m
 
-plus' :: Int -> Int -> Int -> Int
-plus' _ 0 acc = acc
-plus' m n acc = plus' m (n - 1) (acc + 1)
-
--- minus :: Int -> Int -> Int
--- minus m 0 = m
--- minus m n = minus m (n - 1) - 1
-
-minus :: Int -> Int -> Int
-minus m n = minus' m n m
-
-minus' :: Int -> Int -> Int -> Int
-minus' _ 0 acc = acc
-minus' m n acc = minus' m (n - 1) (acc - 1)
-
--- power :: Int -> Int -> Int
--- power m 1 = m
--- power m n = power m (n - 1) * m
-
-power :: Int -> Int -> Int
-power m n = power' m n m
-
-power' :: Int -> Int -> Int -> Int
-power' _ 1 acc = acc
-power' m n acc = power' m (n - 1) (acc * m)
+my_power_iter :: Integer -> Integer -> Integer
+my_power_iter x y = iter x y 1
+  where
+    iter :: Integer -> Integer -> Integer -> Integer
+    iter _ 0 acc = acc
+    iter m n acc = iter m (n - 1) (acc * m)
