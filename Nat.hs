b3009a4ba35a86466e8438c76d0b391e31d8d0d9
Kazu Yamamoto <kazu@iij.ad.jp>
2013-04-30 14:42:24 +0900

    new style.

--- Nat.hs
+++ Nat.hs
@@ -1,37 +1,37 @@
 module Nat (
     Nat
-  , zero
-  , isZero
-  , plus1
-  , minus1
-  , toInt
-  , fromInt
+  , my_zero_n
+  , my_isZero_n
+  , my_plus1_n
+  , my_minus1_n
+  , my_from_n
+  , my_to_n
   ) where
 
 data Nat = Z | S Nat
 
-zero :: Nat
-zero = Z
+my_zero_n :: Nat
+my_zero_n = Z
 
-isZero :: Nat -> Bool
-isZero Z = True
-isZero _ = False
+my_isZero_n :: Nat -> Bool
+my_isZero_n Z = True
+my_isZero_n _ = False
 
-plus1 :: Nat -> Nat
-plus1 = S
+my_plus1_n :: Nat -> Nat
+my_plus1_n = S
 
-minus1 :: Nat -> Nat
-minus1 (S n) = n
-minus1 _     = error "minus1"
+my_minus1_n :: Nat -> Nat
+my_minus1_n (S n) = n
+my_minus1_n _     = error "my_minus1_n"
 
-toInt :: Nat -> Int
-toInt n = toInt' n 0
+my_from_n :: Nat -> Integer
+my_from_n n = iter n 0
   where
-    toInt' m acc
-      | isZero m  = acc
-      | otherwise = toInt' (minus1 m) (acc + 1)
+    iter m acc
+      | my_isZero_n m = acc
+      | otherwise     = iter (my_minus1_n m) (acc + 1)
 
-fromInt :: Int -> Nat
-fromInt 0 = Z
-fromInt n = S (fromInt (n - 1))
+my_to_n :: Integer -> Nat
+my_to_n 0 = Z
+my_to_n n = S (my_to_n (n - 1))
 
