0aaf50085d5f414511d07f1aa4803685a9e8a03f
Kazu Yamamoto <kazu@iij.ad.jp>
2013-08-07 14:55:07 +0900

    adding hs files for Section 10.

---
+++ 10.hs
@@ -0,0 +1,126 @@
+module Main where
+
+import Data.Char
+import Test.Hspec
+
+----------------------------------------------------------------
+
+main :: IO ()
+main = hspec $ do
+    describe "my_paren_seq" $
+        it "counts parenthesis sequentially" $ do
+            my_paren_seq ""       `shouldBe` (0,"")
+            my_paren_seq "()a"    `shouldBe` (1,"a")
+            my_paren_seq "()()()" `shouldBe` (3,"")
+    describe "my_paren_req" $
+        it "counts parenthesis recursively" $ do
+            my_paren_rec ""       `shouldBe` (0,"")
+            my_paren_rec "()a"    `shouldBe` (1,"a")
+            my_paren_rec "((()))" `shouldBe` (3,"")
+    describe "my_paren" $
+        it "counts parenthesis" $ do
+            my_paren ""       `shouldBe` (0,"")
+            my_paren "()a"    `shouldBe` (1,"a")
+            my_paren "()()()" `shouldBe` (3,"")
+            my_paren "((()))" `shouldBe` (3,"")
+            my_paren "((()()))()(()()())" `shouldBe` (9,"")
+    describe "my_expr_simple" $
+        it "calculates a simple mathematical expression" $ do
+            my_expr_simple "1"      `shouldBe` (1,"")
+            my_expr_simple "1+2"    `shouldBe` (3,"")
+            my_expr_simple "1+2+3a" `shouldBe` (6,"a")
+    describe "my_expr" $
+        it "calculates a simple mathematical expression" $ do
+            my_expr "1"             `shouldBe` (1,"")
+            my_expr "1+2"           `shouldBe` (3,"")
+            my_expr "1+2+3a"        `shouldBe` (6,"a")
+            my_expr "(1+2)+3"       `shouldBe` (6,"")
+            my_expr "(1+2)*3"       `shouldBe` (9,"")
+            my_expr "(1+2)*(3+4)+5" `shouldBe` (26,"")
+
+----------------------------------------------------------------
+----------------------------------------------------------------
+
+my_close :: String -> (Int, String)
+my_close (')':left) = (1,left)
+my_close _          = error "no my_close parenthesis"
+
+----------------------------------------------------------------
+-- Counting parenthesis sequentially
+
+my_paren_seq :: String -> (Int, String)
+my_paren_seq ('(':left0) = my_open_seq left0
+my_paren_seq left0       = (0,left0)
+
+my_open_seq :: String -> (Int, String)
+my_open_seq left0 = (cnt1+cnt2, left2)
+  where
+    (cnt1,left1) = my_close    left0
+    (cnt2,left2) = my_paren_seq left1
+
+----------------------------------------------------------------
+-- Counting parenthesis recursively
+
+my_paren_rec :: String -> (Int, String)
+my_paren_rec ('(':left0) = my_open_rec left0
+my_paren_rec left0       = (0,left0)
+
+my_open_rec :: String -> (Int, String)
+my_open_rec left0 = (cnt1+cnt2, left2)
+  where
+    (cnt1,left1) = my_paren_rec left0
+    (cnt2,left2) = my_close    left1
+
+----------------------------------------------------------------
+-- Counting parenthesis generally
+
+my_paren :: String -> (Int, String)
+my_paren ('(':left0) = my_open left0
+my_paren left0       = (0,left0)
+
+my_open :: String -> (Int, String)
+my_open left0 = undefined
+
+----------------------------------------------------------------
+----------------------------------------------------------------
+-- Parser for simple mathematical expressions
+
+{-
+<expr_simple> ::= <nat> (’+’ <expr_simple> | e)
+<nat>         ::= ’0’ | ’1’ | ’2’ | ...
+-}
+
+my_nat :: String -> (Int, String)
+my_nat (x:xs)
+  | isDigit x = (c2i x, xs)
+my_nat _      = error "my_nat"
+
+c2i :: Char -> Int
+c2i x = ord x - ord '0'
+
+my_first :: (a -> c) -> (a,b) -> (c,b)
+my_first f (x,y) = (f x, y)
+
+my_expr_simple :: String -> (Int, String)
+my_expr_simple xs = case my_nat xs of
+    (x, '+':left) -> my_first (x+) $ my_expr_simple left
+    xl            -> xl
+
+----------------------------------------------------------------
+-- Parser for simple mathematical expressions
+
+{-
+<expr>   ::= <term>   (’+’ <expr> | e)
+<term>   ::= <factor> (’*’ <term> | e)
+<factor> ::= ’(’ <expr> ’)’ | <nat>
+<nat>    ::=’0’ | ’1’ | ’2’ | ...
+-}
+
+my_expr :: String -> (Int, String)
+my_expr xs = undefined
+
+my_term :: String -> (Int, String)
+my_term xs = undefined
+
+my_factor :: String -> (Int, String)
+my_factor xs = undefined
---
+++ 10misc.hs
@@ -0,0 +1,76 @@
+module Main where
+
+import Data.Char
+import Text.Parsec
+import Text.Parsec.String
+
+----------------------------------------------------------------
+
+-- | Parser for mathematical expressions
+--
+-- >>> parse my_paren "" "()()()"
+-- Right 3
+-- >>> parse my_paren "" "(((())))"
+-- Right 4
+-- >>> parse my_paren "" "(())(()())"
+-- Right 5
+
+my_paren :: Parser Int
+my_paren = openClose <|> return 0
+  where
+    openClose = do
+        char '('
+        cnt1 <- my_paren
+        char ')'
+        cnt2 <- my_paren
+        return (1 + cnt1 + cnt2)
+
+----------------------------------------------------------------
+
+-- | Parser for mathematical expressions
+--
+-- >>> parse my_expr "" "1"
+-- Right 1
+-- >>> parse my_expr "" "(1+2)*3"
+-- Right 9
+-- >>> parse my_expr "" "(1+2)*(3+4)+5"
+-- Right 26
+
+my_expr :: Parser Int
+my_expr = do
+    n <- my_term
+    m <- plus <|> return 0
+    return (n + m)
+  where
+    plus = do
+        char '+'
+        my_expr
+
+my_term :: Parser Int
+my_term = do
+    n <- my_factor
+    m <- multi <|> return 1
+    return (n * m)
+  where
+    multi = do
+        char '*'
+        my_term
+
+my_factor :: Parser Int
+my_factor = paren <|> my_nat
+  where
+    paren = do
+        char '('
+        n <- my_expr
+        char ')'
+        return n
+
+my_nat :: Parser Int
+my_nat = do
+    n <- oneOf ['0'..'9']
+    return $ ord n - ord '0'
+
+----------------------------------------------------------------
+
+main :: IO ()
+main = undefined
